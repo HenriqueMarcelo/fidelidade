d188e07d961daddf1242f9a4824a762c03333038
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2024-08-10 10:25:12 -0300

    feat: install sass

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header'
+import './Home.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className="home">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quia officia quam sed deleniti id reiciendis
+        amet voluptates aperiam qui voluptas iste, a aspernatur. Dicta cumque quo, nihil atque sequi reiciendis.
+        Necessitatibus, soluta! Corporis quibusdam, magni maiores quis nam earum obcaecati reiciendis quidem dolore, a
+        officia minus atque repudiandae praesentium!
+      </div>
+    </>
+  )
+}
