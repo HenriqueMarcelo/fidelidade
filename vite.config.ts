fe939856d8593edceb21c2216147f6c33d361e69
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2024-08-12 20:00:56 -0300

    test: setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // Habilite o uso de funções globais como describe, it, e expect
+    environment: 'jsdom', // Define o ambiente de teste como jsdom, necessário para testar componentes React
+  },
 })
